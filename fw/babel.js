d62d4d8a8c69b67fda220e8c4f9f254ac117b86c
ivanheral <ivanhernandezal@gmail.com>
2019-10-20 19:21:29 +0200

    clean

--- fw/babel.js
+++ fw/babel.js
@@ -3,22 +3,14 @@ var babel = require('babelify');
 var alias = require('aliasify');
 
 function getBabelRc(ts, fw) {
-
-    var conf = {
+    return {
         presets: ["@babel/env", ...ts ? [...fw.match(/(preact|hyperapp)/i) ? [
             ["@babel/typescript", {
                 jsxPragma: "h"
             }]
         ] : ["@babel/typescript"]] : []],
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        plugins: ts ? [
-            "@babel/proposal-class-properties",
-            "@babel/proposal-object-rest-spread"
-        ] : ["@babel/plugin-syntax-dynamic-import"]
-    };
-
-    if (ts && fw == "angular") {
-        conf.plugins = [
+        plugins: ts ? [...fw.match(/angular/i) ? [
             "babel-plugin-transform-typescript-metadata",
             [
                 "@babel/plugin-proposal-decorators",
@@ -29,12 +21,15 @@ function getBabelRc(ts, fw) {
             ["@babel/proposal-class-properties", {
                 "loose": true
             }]
-        ]
-    }
-    return conf;
+        ] : ["@babel/proposal-class-properties",
+            "@babel/proposal-object-rest-spread"
+        ]] : ["@babel/plugin-syntax-dynamic-import"]
+    };
 }
 
 module.exports = function (fw, is_ts, env) {
+    
+    var babelconf = getBabelRc(is_ts, fw);
 
     var conf = {
         transform: [
@@ -44,17 +39,15 @@ module.exports = function (fw, is_ts, env) {
             }]
         ]
     };
-    if (fw.match(/angular/i)) conf.transform = [...conf.transform, 'angular2-templatify'];
-
+    
     conf.transform = [...conf.transform, ['envify', {
         NODE_ENV: env.match(/prod/i) ? 'production' : 'development',
         global: true
     }]];
 
-    var babelconf = getBabelRc(is_ts, fw);
+    if (fw.match(/angular/i)) conf.transform = [...conf.transform, 'angular2-templatify'];
     if (fw.match(/react/i)) babelconf.presets = [...babelconf.presets, '@babel/react'];
-    if (fw.match(/vue/i)) con.transform = [...conf.transform, vue];
-
+    if (fw.match(/vue/i)) conf.transform = [...conf.transform, vue];
     if (fw.match(/inferno/i))
         babelconf.plugins = [...babelconf.plugins, ["babel-plugin-inferno", {
             "imports": true
@@ -74,7 +67,7 @@ module.exports = function (fw, is_ts, env) {
         }]]
 
     conf.transform = [...conf.transform, babel.configure(babelconf)];
-    if (fw == "vue") {
+    if (fw.match(/vue/i)) {
         conf.transform = [...conf.transform, alias.configure({
             aliases: {
                 vue: `vue/dist/vue.${env.match(/prod/i) && "min"}`
